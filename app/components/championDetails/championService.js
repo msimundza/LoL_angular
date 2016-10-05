4530e6fe1eab98de23dd2ca1d8c4206997550795
msimundza <msimundza@extensionengine.com>
2016-10-05 16:49:32 +0200

    Getting champ tags on champ list (filter on list with checkbox started)

--- app/components/championDetails/championService.js
+++ app/components/championDetails/championService.js
@@ -5,11 +5,9 @@ angular.module('myApp.champion.services', []).
 		LoLChampionDetails.getChampionDetails = function(id) {
 			return $http({
 				method: 'GET',
-				url: 'https://global.api.pvp.net/api/lol/static-data/eune/v1.2/champion/' + id + '?champData=image,lore,skins&api_key=RGAPI-F561B8E9-F25F-4203-B308-BA54A0AF36A4'
+				url: 'https://global.api.pvp.net/api/lol/static-data/eune/v1.2/champion/' + id + '?champData=image,lore,skins,tags&api_key=RGAPI-F561B8E9-F25F-4203-B308-BA54A0AF36A4'
 			});
 		}
 
 		return LoLChampionDetails;
 	}]);
-
-
--- app/components/championsList/championsListService.js
+++ app/components/championsList/championsListService.js
@@ -5,7 +5,7 @@ angular.module('myApp.championsList.services', []).
 		LoLChampionsList.getChampions = function() {
 			return $http({
 				method: 'GET',
-				url: 'https://global.api.pvp.net/api/lol/static-data/eune/v1.2/champion?champData=image&api_key=RGAPI-F561B8E9-F25F-4203-B308-BA54A0AF36A4'
+				url: 'https://global.api.pvp.net/api/lol/static-data/eune/v1.2/champion?champData=image,tags&api_key=RGAPI-F561B8E9-F25F-4203-B308-BA54A0AF36A4'
 			});
 		}
 
