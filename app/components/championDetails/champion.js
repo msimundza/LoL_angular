3315cf4e45914130a998665e9d03d121954c3cae
msimundza <msimundza@extensionengine.com>
2016-10-26 16:04:21 +0200

    Fixed tooltip position

--- app/components/championDetails/champion.js
+++ app/components/championDetails/champion.js
@@ -1,12 +1,13 @@
 'use strict';
 
 angular.module('myApp.champion', ['ngRoute'])
-.controller('championCtrl', ['$scope', '$routeParams', 'LoLChampionDetailsService', function($scope, $routeParams, LoLChampionDetailsService) {
+.controller('championCtrl', ['$scope', '$sce', '$routeParams', 'LoLChampionDetailsService', function($scope, $sce, $routeParams, LoLChampionDetailsService) {
 	$scope.loading = true;
 	$scope.id = $routeParams.id;
 	$scope.championDetails = [];
 	LoLChampionDetailsService.getChampionDetails($scope.id).success(function(resp) {
 		$scope.championDetails = resp;
+		resp.lore = $sce.trustAsHtml(resp.lore);
 	}).error(function(err) {
 		if (err) console.log(err);
 	}).finally(function() {
