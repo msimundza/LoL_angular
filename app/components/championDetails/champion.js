6d2e8b991951d13fb8972ebd4ec23571b452e65a
msimundza <msimundza@extensionengine.com>
2016-10-28 15:51:33 +0200

    Champion name, title added

--- app/components/championDetails/champion.js
+++ app/components/championDetails/champion.js
@@ -4,10 +4,10 @@ angular.module('myApp.champion', ['ngRoute'])
 .controller('championCtrl', ['$scope', '$sce', '$routeParams', 'LoLChampionDetailsService', function($scope, $sce, $routeParams, LoLChampionDetailsService) {
 	$scope.loading = true;
 	$scope.id = $routeParams.id;
-	$scope.championDetails = [];
+
 	LoLChampionDetailsService.getChampionDetails($scope.id).success(function(resp) {
 		$scope.championDetails = resp;
-		resp.lore = $sce.trustAsHtml(resp.lore);
+		$scope.championDetails.lore = $sce.trustAsHtml(resp.lore);
 	}).error(function(err) {
 		if (err) console.log(err);
 	}).finally(function() {
