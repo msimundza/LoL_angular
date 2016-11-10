38c4a0cbdda9d44ea62c4fddfec9fabe756ab5c5
Marko Simundza <markosimundza@gmail.com>
2016-11-11 00:37:22 +0100

    Different styling for skin preview, added: support for smaller screens, skin name under image, read more functionality

--- app/components/championDetails/champion.js
+++ app/components/championDetails/champion.js
@@ -3,16 +3,31 @@
 angular.module('myApp.champion', ['ngRoute'])
 .controller('championCtrl', ['$scope', '$sce', '$routeParams', 'LoLChampionDetailsService', function($scope, $sce, $routeParams, LoLChampionDetailsService) {
 	$scope.loading = true;
+	$scope.collapsed = true;
 	$scope.id = $routeParams.id;
 	$scope.imageNum = 0;
+	$scope.bla = '';
 
-	$scope.setImage = function (num) {
+	$scope.setImageAndSkinName = function (num, skinName) {
 		$scope.imageNum = num;
+		// Show champ name instead of 'default' string
+		if (skinName != 'default') {
+			return $scope.championDetails.skinName = skinName;
+		}
+		$scope.championDetails.skinName = $scope.championDetails.defaultSkinName;
 	};
+
+	$scope.toggleLore = function (collapsed) {
+		$scope.collapsed = !collapsed;
+	}
 
 	LoLChampionDetailsService.getChampionDetails($scope.id).success(function(resp) {
 		$scope.championDetails = resp;
 		$scope.championDetails.lore = $sce.trustAsHtml(resp.lore);
+		$scope.championDetails.blurb = $sce.trustAsHtml(resp.blurb);
+		$scope.championDetails.skinName = resp.name;
+		$scope.championDetails.defaultSkinName = resp.name;
+		console.log(resp);
 	}).error(function(err) {
 		if (err) console.log(err);
 	}).finally(function() {
